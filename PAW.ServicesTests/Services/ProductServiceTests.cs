9c472cdd70a8796eb5b19ef3845bf89985a70706
bryandavila <badmdavila@gmail.com>
2025-04-06 22:12:02 -0600

    Nuevas caracteristicas

---
+++ PAW.ServicesTests/Services/ProductServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+using PAW.Services;
+using PAW.Models.Products;
+using Moq;
+using System.Collections.Generic;
+using Task = System.Threading.Tasks.Task;
+using PAW.Architecture.Providers;
+using APW.Architecture;
+
+namespace PAW.ServicesTests.Services
+{
+    public class ProductServiceTests
+    {
+        [Fact]
+        public async Task GetFiveFromFactoryAsync_ShouldReturnFiveProducts()
+        {
+            // Arrange
+            var mockRestProvider = new Mock<IRestProvider>();
+            var service = new ProductService(mockRestProvider.Object);
+
+            // Act
+            var result = await service.GetFiveFromFactoryAsync();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(5, new List<Product>(result).Count);
+        }
+    }
+}
